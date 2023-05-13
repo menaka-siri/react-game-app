8a4e22856f39c5da9c97a891b36dedae1ea98c89
Menaka Sirisena <menaka.12@cse.mrt.ac.lk>
2023-05-14 04:34:04 +0530

    use useState to update variable state

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,31 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 function ListGroup() {
-  let  items = ["New York", "San Francisco", "Tokyo", "France"];
-    items = []
+  let items = ["New York", "San Francisco", "Tokyo", "France"];
+  
+  //Hook
+  // useState returns an array
+  // arr[0] = variable
+  // arr[1] = updater function
+   
+  const [selectedIndex, setSelectedIndex] = useState(-1)
+
+  //Event handler
+  //const handleClick = (event: MouseEvent) => console.log(event)
 
   return (
     <Fragment>
       <h1>Test Block</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={ selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
+            key={item}
+            onClick={() => { setSelectedIndex(index) }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </Fragment>
